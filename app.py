2d9f86ffc0ade6116a9a4a6ad34933af69a62320
Robbins <robbinsx@msoe.edu>
2024-02-05 20:50:41 -0600

    working flask app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello peoples'
 
-@app.route("/hehehe")
-def spongebob_gif():
-    return send_from_directory("static", "spongebob.mp4")
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
